a2d8834be0dadd531ff43ba9ccb16dc027a2ff08
scott@cassidylabs.com <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-02-19 09:06:15 -0800

    Add generated grid.

---
+++ js.js
@@ -0,0 +1,48 @@
+
+// select the three button elements
+let sixtyFour = document.getElementById("64");
+let oneHundred = document.getElementById("100");
+let resetButton = document.getElementById("reset");
+let lastCanvasSize;
+
+//add event listeners to my 3 buttons
+sixtyFour.addEventListener("click",  () => lastCanvasSize = 20)
+sixtyFour.addEventListener("click", () => populateScreen(lastCanvasSize));
+
+oneHundred.addEventListener("click",  () => lastCanvasSize = 40)
+oneHundred.addEventListener("click", () => populateScreen(lastCanvasSize));
+
+resetButton.addEventListener("click", () => populateScreen(lastCanvasSize));
+
+//start a new canvas
+function populateScreen (num) {
+    console.log(lastCanvasSize);
+    console.log(num);
+    let screen = document.querySelector(".screen");
+    screen.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    screen.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    let generated = document.querySelectorAll(".generated");
+    
+    generated.forEach(element => {
+        element.remove();
+    });
+
+
+    for (let i = 0; i < num * num; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = 'white';
+        square.style.border = "1px solid black"
+        square.style.borderColor = "rgba(0,0,0,0.025)"
+        square.setAttribute("class", "generated");
+        screen.insertAdjacentElement("beforeend", square);
+    }
+}
+
+populateScreen(lastCanvasSize)
+
+function testFunction() {
+    console.log("button heard");
+}
+function resetFunction() {
+    console.log("Reset button is heard");
+}
